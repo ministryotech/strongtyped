d804b5e663276b5c58b47476da5ef2c961202609
Keith Jackson <keith@ministryotech.co.uk>
2014-11-03 16:58:55 +0000

    Migrated over some extension and conversion helpers from the old MOT Project

---
+++ Ministry.StrongTyped/Conversions.cs
@@ -0,0 +1,216 @@
+﻿// Copyright (c) 2014 Minotech Ltd.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Ministry.StrongTyped
+{
+    /// <summary>
+    /// A static class of methods to convert types which can be used directly or as extensions.
+    /// </summary>
+    public static class Conversions
+    {
+
+        #region | Boolean Conversion Methods |
+
+        #region | ToInt32 |
+
+        /// <summary>
+        /// Converts a boolean to an integer value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="trueValue">The true value.</param>
+        /// <param name="falseValue">The false value.</param>
+        /// <returns>
+        /// The appropriate integer value.
+        /// </returns>
+        public static int ToInt32(this bool value, int trueValue = 1, int falseValue = 0)
+        {
+            return (value ? trueValue : falseValue);
+        }
+
+        /// <summary>
+        /// Converts a boolean to an integer value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="trueValue">The integer value to return if the boolean value is true.</param>
+        /// <param name="falseValue">The false value.</param>
+        /// <returns>
+        /// The appropriate integer value.
+        /// </returns>
+        public static int? ToInt32(this bool? value, int trueValue = 1, int falseValue = 0)
+        {
+            if (value.HasValue)
+            {
+                return value.Value.ToInt32(trueValue, falseValue);
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region | ToString |
+
+        /// <summary>
+        /// Converts a boolean to a string value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="stringType">Type of the string.</param>
+        /// <returns>
+        /// The appropriate string value.
+        /// </returns>
+        /// <remarks>
+        /// To convert to 1/0 call ToInt().
+        /// </remarks>
+        public static string ToString(this bool value, BooleanToStringConversionType stringType)
+        {
+            switch (stringType)
+            {
+                case BooleanToStringConversionType.TrueOrFalse:
+                    return (value ? "True" : "False");
+                case BooleanToStringConversionType.YesOrNo:
+                    return (value ? "Yes" : "No");
+                case BooleanToStringConversionType.YOrN:
+                    return (value ? "Y" : "N");
+                default:
+                    return String.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Converts a boolean to a string value of a specified type.
+        /// </summary>
+        /// <param name="value">The boolean value to convert from.</param>
+        /// <param name="stringType">Type of the string.</param>
+        /// <returns>
+        /// The appropriate string value.
+        /// </returns>
+        /// <remarks>
+        /// To convert to 1/0 call ToInt().
+        /// </remarks>
+        public static string ToString(this bool? value, BooleanToStringConversionType stringType)
+        {
+            return value.HasValue ? value.Value.ToString(stringType) : String.Empty;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region | Integer Conversions |
+
+        /// <summary>
+        /// Converts an integer to it's boolean value.
+        /// </summary>
+        /// <param name="value">The integer to convert from.</param>
+        /// <returns>The appropriate boolean value.</returns>
+        /// <remarks>Converts 0 False, all else True.</remarks>
+        public static bool ToBoolean(this int value)
+        {
+            return (value > 0);
+        }
+
+        #endregion
+
+        #region | String Conversions |
+
+        /// <summary>
+        /// Converts a string to it's boolean value.
+        /// </summary>
+        /// <param name="value">The string to convert from.</param>
+        /// <returns>The appropriate boolean value.</returns>
+        /// <remarks>Converts 1/0, True/False, Yes/No, etc. Returns nothing otherwise.</remarks>
+        /// <exception cref="System.InvalidCastException">Thrown if the string cannot be transformed into a boolean value.</exception>
+        public static bool ToBoolean(this string value)
+        {
+            switch (value.Trim().ToUpper(CultureInfo.InvariantCulture))
+            {
+                case "1":
+                    return true;
+                case "0":
+                    return false;
+                case "-1":
+                    return false;
+                case "YES":
+                    return true;
+                case "NO":
+                    return false;
+                case "TRUE":
+                    return true;
+                case "FALSE":
+                    return false;
+                case "Y":
+                    return true;
+                case "N":
+                    return false;
+                default:
+                    throw new InvalidCastException("The object passed cannot be converted");
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Converts a generic list of any type to a delimited list of string values.
+        /// </summary>
+        /// <param name="value">The strings to delimit.</param>
+        /// <param name="delimiter">The delimiter to split the strings by.</param>
+        /// <returns>The delimited string.</returns>
+        /// <exception cref="System.ArgumentNullException">The list passed in is null.</exception>
+        public static string ToDelimitedString(this IList value, string delimiter = ", ")
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (delimiter == null) throw new ArgumentNullException("delimiter");
+
+            var builder = new StringBuilder();
+
+            foreach (var item in value)
+            {
+                if (item.ToString().IsNullOrEmpty()) continue;
+                builder.Append(item);
+                builder.Append(delimiter);
+            }
+
+            return (builder.Length > 0 ? builder.ToString().RemoveFromEnd(delimiter.Length) : String.Empty);
+        }
+
+        /// <summary>
+        /// Converts a hashtable of strings to a delimited list.
+        /// </summary>
+        /// <param name="value">The strings to delimit.</param>
+        /// <param name="delimiter">The delimiter to split the strings by.</param>
+        /// <param name="excludeKeys">A flag to indicate if keys should be excluded.</param>
+        /// <returns>
+        /// The delimited string.
+        /// </returns>
+        /// <exception cref="System.ArgumentNullException">The Hashtable passed in is null.</exception>
+        public static string ToDelimitedString(this IDictionary value, string delimiter = ", ", bool excludeKeys = false)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (delimiter == null) throw new ArgumentNullException("delimiter");
+
+            var tempList = (from DictionaryEntry entry 
+                            in value 
+                            select (excludeKeys ? String.Empty : entry.Key + ":") + entry.Value).ToList();
+
+            tempList.Sort();
+            return tempList.ToDelimitedString(delimiter);
+        }
+
+    }
+}
---
+++ Ministry.StrongTyped/StringHelper.cs
@@ -0,0 +1,232 @@
+﻿// Copyright (c) 2014 Minotech Ltd.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+// so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Ministry.StrongTyped
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Adds spaces into a string where capital letters occur.
+        /// </summary>
+        /// <param name="value">The string to change.</param>
+        /// <returns>The altered string.</returns>
+        /// <remarks>As an example TheQuickBrownFox would become The Quick Brown Fox.</remarks>
+        public static string AddSpacesByCasing(this string value)
+        {
+            return value.AddCharactersByCasing(" ");
+        }
+
+        /// <summary>
+        /// Adds characters into a string where capital letters occur.
+        /// </summary>
+        /// <param name="value">The string to change.</param>
+        /// <param name="insertCharacters">The characters to insert.</param>
+        /// <returns>The altered string.</returns>
+        /// <remarks>As an example TheQuickBrownFox would become The|Quick|Brown|Fox, should '|' be used as the insert character.</remarks>
+        public static string AddCharactersByCasing(this string value, string insertCharacters)
+        {
+            var retVal = new StringBuilder();
+            foreach (var stringVal in value.Select(c => c.ToString(CultureInfo.InvariantCulture)))
+            {
+                if ((stringVal == stringVal.ToUpper(CultureInfo.InvariantCulture)) && (retVal.ToString().IsNotNullOrEmpty())) retVal.Append(insertCharacters);
+                retVal.Append(stringVal);
+            }
+            return retVal.ToString();
+        }
+
+        /// <summary>
+        /// Appends a value to the StringBuilder if it is currently empty.
+        /// </summary>
+        /// <param name="value">The stringbuilder to append to.</param>
+        /// <param name="appendValue">The value to append.</param>
+        /// <returns>The completed builder.</returns>
+        /// <exception cref="System.ArgumentNullException">Either the StringBuilder or value to append are null.</exception>
+        public static StringBuilder AppendIfEmpty(this StringBuilder value, string appendValue)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (appendValue == null) throw new ArgumentNullException("appendValue");
+
+            if (value.Length == 0) value.Append(appendValue);
+            return value;
+        }
+
+        /// <summary>
+        /// Appends a value to the StringBuilder if it is not currently empty.
+        /// </summary>
+        /// <param name="value">The stringbuilder to append to.</param>
+        /// <param name="appendValue">The value to append.</param>
+        /// <returns>The completed builder.</returns>
+        /// <exception cref="System.ArgumentNullException">Either the StringBuilder or value to append are null.</exception>
+        public static StringBuilder AppendIfNotEmpty(this StringBuilder value, string appendValue)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (appendValue == null) throw new ArgumentNullException("appendValue");
+
+            if (value.Length > 0) value.Append(appendValue);
+            return value;
+        }
+
+        /// <summary>
+        /// Determines if this instance is Null or Empty.
+        /// </summary>
+        /// <param name="value">The string to evaluate.</param>
+        /// <returns>A flag to indicate the result.</returns>
+        /// <remarks>This uses the static String.IsNullOrEmpty method.</remarks>
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return String.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Determines if this instance is Null or Empty.
+        /// </summary>
+        /// <param name="value">The string to evaluate.</param>
+        /// <returns>A flag to indicate the result.</returns>
+        /// <remarks>This uses the static String.IsNullOrEmpty method.</remarks>
+        public static bool IsNotNullOrEmpty(this string value)
+        {
+            return !value.IsNullOrEmpty();
+        }
+
+        /// <summary>
+        /// Removes a set number of characters from the end of a string.
+        /// </summary>
+        /// <param name="value">The string to change.</param>
+        /// <param name="characterCount">The number of characters to remove.</param>
+        /// <returns>The altered string.</returns>
+        /// <exception cref="System.ArgumentNullException">The string passed to the value parameter is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">The characterCount parameter must be non-negative and less than value.Length.</exception>
+        public static string RemoveFromEnd(this string value, int characterCount)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (characterCount < 0) throw new ArgumentOutOfRangeException("characterCount");
+            if (characterCount > value.Length) throw new ArgumentOutOfRangeException("characterCount");
+
+            return value.Remove(value.Length - (characterCount), characterCount);
+        }
+
+        /// <summary>
+        /// Removes a set number of characters from the end of a StringBuilder.
+        /// </summary>
+        /// <param name="value">The StringBuilder to change.</param>
+        /// <param name="characterCount">The number of characters to remove.</param>
+        /// <returns>The altered StringBuilder.</returns>
+        /// <exception cref="System.ArgumentNullException">The StringBuilder passed to the value parameter is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">The characterCount parameter must be non-negative and less than value.Length.</exception>
+        public static StringBuilder RemoveFromEnd(this StringBuilder value, int characterCount)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (characterCount < 0) throw new ArgumentOutOfRangeException("characterCount");
+            if (characterCount > value.Length) throw new ArgumentOutOfRangeException("characterCount");
+
+            if (value.Length > 0)
+            {
+                return value.Length - characterCount > 0 
+                    ? value.Remove(value.Length - (characterCount), characterCount) 
+                    : new StringBuilder(string.Empty);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// Removes a set number of characters from the start of a string.
+        /// </summary>
+        /// <param name="value">The string to change.</param>
+        /// <param name="characterCount">The number of characters to remove.</param>
+        /// <returns>The altered string.</returns>
+        /// <exception cref="System.ArgumentNullException">The string passed to the value parameter is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">The characterCount parameter must be non-negative and less than value.Length.</exception>
+        public static string RemoveFromStart(this string value, int characterCount)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            return value.Remove(0, characterCount);
+        }
+
+        /// <summary>
+        /// Removes a set number of characters from the start of a StringBuilder.
+        /// </summary>
+        /// <param name="value">The StringBuilder to change.</param>
+        /// <param name="characterCount">The number of characters to remove.</param>
+        /// <returns>The altered StringBuilder.</returns>
+        /// <exception cref="System.ArgumentNullException">The StringBuilder passed to the value parameter is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">The characterCount parameter must be non-negative and less than value.Length.</exception>
+        public static StringBuilder RemoveFromStart(this StringBuilder value, int characterCount)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (characterCount < 0) throw new ArgumentOutOfRangeException("characterCount");
+            if (characterCount > value.Length) throw new ArgumentOutOfRangeException("characterCount");
+
+            if (value.Length > 0)
+            {
+                return value.Length - characterCount > 0 
+                    ? value.Remove(0, characterCount) 
+                    : new StringBuilder(string.Empty);
+            }
+            
+            return value;
+        }
+
+        /// <summary>
+        /// Converts a string to an array.
+        /// </summary>
+        /// <param name="value">The string to convert.</param>
+        /// <param name="delimiter">The delimiter.</param>
+        /// <returns>An array of strings.</returns>
+        /// <exception cref="System.ArgumentNullException">The string provided to the 'value' parameter is null.</exception>
+        /// <exception cref="System.ArgumentException">The string delimiter provided must be at least 1 character long.</exception>
+        public static string[] Split(this string value, string delimiter)
+        {
+            if (value == null) throw new ArgumentNullException("value");
+            if (delimiter.IsNullOrEmpty()) throw new ArgumentException("The delimiter specified was invalid", "delimiter");
+
+            var delim = ':';
+
+            if (value.Contains(":"))
+            {
+                if (!value.Contains("|"))
+                {
+                    delim = '|';
+                }
+                else if (!value.Contains("^"))
+                {
+                    delim = '^';
+                }
+                else if (!value.Contains("="))
+                {
+                    delim = '=';
+                }
+                else if (!value.Contains("/"))
+                {
+                    delim = '/';
+                }
+                else if (!value.Contains("-"))
+                {
+                    delim = '-';
+                }
+                else
+                {
+                    delim = delimiter[0];
+                }
+            }
+
+            var tmp = value.Replace(delimiter, delim.ToString(CultureInfo.InvariantCulture));
+            return tmp.Split(delim);
+        }
+    }
+}
